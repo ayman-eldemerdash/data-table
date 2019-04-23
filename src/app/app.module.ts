d60a432bd699547d1b70b8e9f7896382812821d7
ayman <ayman.el-demerdash@outlook.com>
2019-04-23 19:24:30 +0200

    done more optimzed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,34 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+// Angular Material Modules --> should be in a separated module
 import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatInputModule } from '@angular/material/input';
 import { MatSortModule } from '@angular/material/sort';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
+// Components
+import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { TableComponent } from './components/table/table.component';
+
+// Services
 import { UsersService } from './services/users.service';
 
-
+// for stackblitz only
+import 'hammerjs';
+import 'bootstrap';
+import 'jquery';
+import 'popper.js';
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    SearchBarComponent,
-    TableComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
@@ -38,6 +41,12 @@ import { UsersService } from './services/users.service';
     MatInputModule,
     MatSortModule,
     MatToolbarModule
+  ],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SearchBarComponent,
+    TableComponent
   ],
   providers: [UsersService],
   bootstrap: [AppComponent]
